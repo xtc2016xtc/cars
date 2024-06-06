e9e4df55174ad14b6349d052132e61f4b9f04427
xtc2016xtc <728611827@qq.com>
2024-06-06 16:18:03 +0800

    1.50.07

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default async function Home() {
         {!isDataEmpty ? (
           <section>
            <div className="home__cars-wrapper">
-              {allCars?.map((car)=><CarCard car={car} key="null"/>) }
+              {allCars?.map((car)=><CarCard car={car} />) }
            </div>
           </section>
         ):(
--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import { CarProps } from '@/types';
 import CustomButton from './CustomButton';
 import { calculateCarRent } from '@/utils';
+import CarDetails from './CarDetails';
 
 interface CarCardProps {
   car:CarProps;
@@ -12,7 +13,11 @@ interface CarCardProps {
 const CarCard = ({car}:CarCardProps) => {
   const { city_mpg , year , make , model , transmission ,drive } = car
 
+  const [isOpen, setIsOpen] = useState(false);
+
   const carRent = calculateCarRent(city_mpg,year)
+
+
 
   return (
     <div className='car-card group' >
@@ -42,9 +47,57 @@ const CarCard = ({car}:CarCardProps) => {
         />
       </div>
 
-      <div className="">
-        
+      <div className="relative flex w-full mt-2">
+        <div className='flex group-hover:invisible w-full justify-between text-gray'>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image 
+              src="/steering-wheel.svg"
+              width={20}
+              height={20}
+              alt='steering wheel'
+            />
+            <p className='text-[14px]'>
+              {transmission === 'a' ? 'Automatic':'Manual'}
+            </p>
+          </div>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image 
+              src="/tire.svg"
+              width={20}
+              height={20}
+              alt='tire'
+            />
+            <p className='text-[14px]'>
+              {drive.toUpperCase()}
+            </p>
+          </div>
+          <div className='flex flex-col justify-center items-center gap-2'>
+            <Image 
+              src="/gas.svg"
+              width={20}
+              height={20}
+              alt='steering wheel'
+            />
+            <p className='text-[14px]'>
+              {city_mpg} MPG
+            </p>
+          </div>
+        </div>
+
+        <div className='car-card__btn-container'>
+          <CustomButton
+            title='View More' 
+            btnType={'button'}
+            containerStyles='w-full py-[16px] rounded-full bg-primary-blue'
+            textStyles='text-white text-[14px] leading-[17px] font-bold'
+            rightIcon="/right-arrow.svg"
+            handleClick={()=>setIsOpen(true)}
+            />
+        </div>
       </div>
+
+      <CarDetails isOpen={isOpen} closeModal={
+        ()=>setIsOpen(false)} car={car}/>
     </div>
   )
 }
