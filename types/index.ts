f1f562b5f5fdc33a6003aecade517fa1abe6dfc9
xtc2016xtc <728611827@qq.com>
2024-07-20 09:43:39 +0800

    修改bug

--- types/index.ts
+++ types/index.ts
@@ -55,11 +55,13 @@ export interface OptionProps {
 export interface CustomFilterProps {
   title: string;
   options: OptionProps[];
+  setFilter: (filter: string) => void;
 }
 
 export interface ShowMoreProps {
   pageNumber: number;
   isNext: boolean;
+  setLimit: (limit: number) => void;
 }
 
 export interface SearchManuFacturerProps {
